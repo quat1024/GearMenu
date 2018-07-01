64d75b66dc1f498536c0a08f3fb47f3f25986d11
quat <scratch933@gmail.com>
2018-06-30 21:43:01 -0400

    Bye Felicia

--- src/main/java/projgear/gearmenu/gui/event/IEvent.java
+++
@@ -1,5 +0,0 @@
-package projgear.gearmenu.gui.event;
-
-public interface IEvent<O> {
-	void run(O item);
-}
--- src/main/java/projgear/gearmenu/gui/math/MathUtil.java
+++
@@ -1,7 +0,0 @@
-package projgear.gearmenu.gui.math;
-
-public class MathUtil {
-	public static double rangeRemap(double value, double low1, double high1, double low2, double high2) {
-		return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
-	}
-}
